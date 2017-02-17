9687510dd709f9427ea6600c6d8d44f15f9a6b29
Ricardo Velhote <rvelhote@gmail.com>
2017-02-17 22:16:05 +0000

    Test the WebSocket handler

--- application/dnspropagation_test.go
+++ application/dnspropagation_test.go
@@ -23,8 +23,10 @@ package application
  * SOFTWARE.
  */
 import (
+	"github.com/gorilla/websocket"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"strings"
 	"testing"
 )
@@ -47,5 +49,88 @@ func TestIndexHandler(t *testing.T) {
 	expected := `<main id="app"></main>`
 	if strings.Contains(expected, rr.Body.String()) {
 		t.Error("the response should contain a <main> html tag")
+	}
+}
+
+// TODO Improve this thing!
+func makeWebSocketRequest(path string, withOrigin bool) (*websocket.Conn, *http.Response, *httptest.Server) {
+	//
+	dialer := websocket.Dialer{}
+
+	config := Configuration{Servers: []Server{testServer}}
+	queryHandler := QueryRequestHandler{Configuration: config}
+
+	http.Handle(path, queryHandler)
+	server := httptest.NewServer(nil)
+
+	header := http.Header{"Origin": []string{"http://127.0.0.99:8080"}}
+	if withOrigin {
+		header = http.Header{"Origin": []string{"http://127.0.0.1:8080"}}
+	}
+
+	urlll := &url.URL{Host: server.Listener.Addr().String(), Scheme: "ws", Path: path}
+	conn, httpResp, _ := dialer.Dial(urlll.String(), header)
+
+	return conn, httpResp, server
+}
+
+func TestWebsocketHandler(t *testing.T) {
+	conn, _, server := makeWebSocketRequest("/api/v1/query", true)
+
+	defer conn.Close()
+	defer server.Close()
+
+	conn.WriteJSON(WebsocketRequest{Domain: "golang.org", RecordType: "a"})
+
+	response := Response{}
+	conn.ReadJSON(&response)
+
+	if len(response.Records.Data) == 0 {
+		t.Error("The WebSocket should have replied with some DNS Records but the response data was empty")
+	}
+}
+
+func TestWebsocketHandlerFailedDomainValidation(t *testing.T) {
+	conn, _, server := makeWebSocketRequest("/api/v2/query", true)
+
+	defer conn.Close()
+	defer server.Close()
+
+	conn.WriteJSON(WebsocketRequest{Domain: "", RecordType: "a"})
+
+	responseError := ResponseError{}
+	conn.ReadJSON(&responseError)
+
+	if responseError.Error != ErrInvalidDomain.Error() {
+		t.Errorf("The WebSocket should have replied with -- %s -- but it replied -- %s --", ErrInvalidDomain, responseError.Error)
+	}
+}
+
+func TestWebsocketHandlerFailedRecordValidation(t *testing.T) {
+	conn, _, server := makeWebSocketRequest("/api/v3/query", true)
+
+	defer conn.Close()
+	defer server.Close()
+
+	conn.WriteJSON(WebsocketRequest{Domain: "golang.org", RecordType: "XYZT"})
+
+	responseError := ResponseError{}
+	conn.ReadJSON(&responseError)
+
+	if responseError.Error != ErrInvalidRecord.Error() {
+		t.Errorf("The WebSocket should have replied with -- %s -- but it replied -- %s --", ErrInvalidRecord, responseError.Error)
+	}
+}
+
+func TestWebsocketHandlerBadOrigin(t *testing.T) {
+	conn, response, server := makeWebSocketRequest("/api/v4/query", false)
+	defer server.Close()
+
+	if conn != nil {
+		t.Error("There should not be a connection from the WebSocket request but there is one")
+	}
+
+	if response.StatusCode != 403 {
+		t.Errorf("The server returned HTTP Status Code -- %d -- but it should have returned a 403", response.StatusCode)
 	}
 }
