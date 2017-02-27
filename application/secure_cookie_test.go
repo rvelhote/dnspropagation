5c92411eb52081b8c34e1fbfaa79893d71272837
Ricardo Velhote <rvelhote@gmail.com>
2017-02-27 20:43:29 +0000

    Added tests to the secure cookie

--- application/secure_cookie_test.go
+++ application/secure_cookie_test.go
@@ -1,6 +1,13 @@
 // Package application contains the server-side application code.
 package application
 
+import (
+    "testing"
+    "github.com/gorilla/securecookie"
+    "encoding/base64"
+    "net/http"
+)
+
 /*
  * The MIT License (MIT)
  *
@@ -22,3 +29,36 @@ package application
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
+const cookieName = "recaptcha"
+const testHashKey = "RovMQmutMbSogUuGQFZYLb37jwgwFNuMR7wrEz9EILQ9W039UHCFlCfkpX1EbecktHA563XX+7clPRinBPeaeQ=="
+const testBlockKey = "+sSXCAbwswiYNqHx4zCuJJTD3hmRQp4f4uJKy+aFL70="
+
+func generateGorillaSecureCookie() *securecookie.SecureCookie {
+    hashKey, _ := base64.StdEncoding.DecodeString(testHashKey)
+    blockKey, _ := base64.StdEncoding.DecodeString(testBlockKey)
+    return securecookie.New(hashKey, blockKey)
+}
+
+func TestNewSecureRecaptchaCookie(t *testing.T) {
+    secureRecaptchaCookie1 := NewSecureRecaptchaCookie(cookieName, nil, generateGorillaSecureCookie())
+    if secureRecaptchaCookie1.Name != cookieName || len(secureRecaptchaCookie1.Value) != 0 {
+        t.Error("The new secure cookie based on an empty cookie should have no value")
+    }
+
+    validCookie := &http.Cookie{Value: "Some Value"}
+    secureRecaptchaCookie2 := NewSecureRecaptchaCookie(cookieName, validCookie, generateGorillaSecureCookie())
+    if secureRecaptchaCookie2.Name != cookieName || len(secureRecaptchaCookie2.Value) == 0 {
+        t.Error("The new secure cookie based on a valid cookie should have a value")
+    }
+}
+
+func TestSecureRecaptchaCookie_Encode(t *testing.T) {
+    validCookie := &http.Cookie{Value: "Some Value"}
+
+    secureRecaptchaCookie := NewSecureRecaptchaCookie(cookieName, validCookie, generateGorillaSecureCookie())
+    secureRecaptchaCookie.Value = secureRecaptchaCookie.Encode(validCookie.Value)
+
+    if secureRecaptchaCookie.IsValid(validCookie.Value) != true {
+        t.Error("The cookie value should have been encoded and decoded correctly")
+    }
+}
